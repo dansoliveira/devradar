6e6df70a32bb299cf0ab30615e99ff1771e9a23d
Daniel Oliveira <oliveiradaniel@protonmail.com>
2020-01-17 01:46:35 -0300

    feat(backend): add list devs

--- backend/src/app/controllers/DevController.js
+++ backend/src/app/controllers/DevController.js
@@ -2,6 +2,12 @@ import axios from 'axios';
 import Dev from '../models/Dev';
 
 class DevController {
+  async index(request, response) {
+    const devs = await Dev.find();
+
+    return response.json(devs);
+  }
+  
   async store(request, response) {
     const { github_username, techs, latitude, longitude } = request.body;
 
